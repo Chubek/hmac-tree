31c723928c114547fc658fd24e99d87d6600dd16
Chubak <chubakbidpaa@gmail.com>
2022-05-22 20:53:29 +0430

    done: serializer

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,9 +1,18 @@
-use hmac_tree::hmac::Hmac;
+use hmac_procs::htree_json;
+use hmac_tree::encoders::serializer::HtreeJsonSerializer;
+
+#[htree_json(ignore = [bb])]
+pub struct T {
+    tt: Vec<u8>,
+    bb: String,
+}
 
 
 fn main() {
-    let hmac = Hmac::from_str("lll", "sss");
+    let t = T { tt: vec![0u8, 38u8], bb: "SSS".to_string() };
 
-    println!("{}", hmac.calculate().1)
+    let j = t.ser_into_json();
+
+    println!("{}", String::from_utf8(j).unwrap())
 
 }
