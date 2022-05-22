2e64f9737f1a2611f886efeda438d143a64cdd64
Chubak <chubakbidpaa@gmail.com>
2022-05-23 04:24:58 +0430

    added: htree index

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,18 +1,13 @@
-use hmac_procs::htree_json;
-use hmac_tree::encoders::serializer::HtreeJsonSerializer;
+use std::str::FromStr;
 
-#[htree_json(ignore = [bb])]
-pub struct T {
-    tt: Vec<u8>,
-    bb: String,
-}
-
+use proc_macro2::TokenStream;
+use syn::parse2;
 
 fn main() {
-    let t = T { tt: vec![0u8, 38u8], bb: "SSS".to_string() };
+    let t = TokenStream::from_str("{ println!(\"sss\") }").unwrap();
+    let p = parse2::<syn::Expr>(t).unwrap();
 
-    let j = t.ser_into_json();
-
-    println!("{}", String::from_utf8(j).unwrap())
-
+    if let syn::Expr::Block(b) = p {
+        b.
+    }
 }
